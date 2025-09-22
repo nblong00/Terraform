95eeebe8eddede7a933952d68432692d9b29522f
Blaise <blazzze0517@gmail.com>
2025-09-21 20:34:27 -0400

    Adjusted formatting.

--- Development_ENV/Compute.tf
+++ Development_ENV/Compute.tf
@@ -1,6 +1,7 @@
 data "aws_ami" "base_ami_id" {
-    owners           = ["amazon"]
-    most_recent      = true
+  owners      = ["amazon"]
+  most_recent = true
+
   filter {
     name   = "name"
     values = ["al2023-ami-2023*-x86_64"]
@@ -8,9 +9,10 @@ data "aws_ami" "base_ami_id" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = data.aws_ami.base_ami_id.id
+  ami           = data.aws_ami.base_ami_id.id
   instance_type = "t2.micro"
-  count = 3
+  count         = 3
+  
   tags = {
     name = "Dev_Instance[${count.index}]"
   }
